ab3645a6793a57571c1bf6548c7b36fcc9e010f5
amciro <amciro@misena.edu.co>
2011-11-25 22:32:26 +0000

    Se organizaron algunos errores del crud de piloto y se adiciono una tabla aunque aun no funciona

--- menus/src/Piloto.java
+++ menus/src/Piloto.java
@@ -1,44 +1,55 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+import javax.swing.table.AbstractTableModel;
+
+
+
 import java.util.ArrayList;
 
 public class Piloto extends JFrame {
-
-	
-	
-	
 	
 
-	public void agregarEs(ClasePiloto cl) {
-		
+
+	public void agregar(ClasePiloto cl) {
+
 		Menu.listaP.add(cl);
 	}
-
-	public void igualesEs(ClasePiloto cls) {
+	
+	        
+	public void iguales(ClasePiloto as) {
 		int re = 0;
-		for (ClasePiloto ad : Menu.listaP) {
-			if (ad.getIdentificacion() == (cls.getIdentificacion())) {
+		for (ClasePiloto p : Menu.listaP) {
+			if (p.getIdentificacion().equals(as.getIdentificacion())) {
+				System.out
+						.println("ya existe un piloto con el mismo numero de identificacion");
 				JOptionPane.showMessageDialog(null,
-						"ya existe un piloto con la misma identificacion");
+				"ya existe un piloto con el mismo numero de identificacion");
 				re = 1;
 			}
-
 		}
-
 		if (re == 0)
-			agregarEs(cls);
-		JOptionPane.showMessageDialog(null, "piloto Creado con Exito");
+			agregar(as);
+			
 	}
+	
+	
+	
+	
 
 	public void buscarE(ClasePiloto asd) {
 		int oe = 1;
@@ -46,7 +57,7 @@ public class Piloto extends JFrame {
 			JOptionPane.showMessageDialog(null, "no hay datos");
 		} else {
 			for (ClasePiloto f : Menu.listaP) {
-				if (f.getIdentificacion() == (asd.getIdentificacion())) {
+				if (f.getIdentificacion().equals(asd.getIdentificacion())) {
 					System.out.println(f);
 					oe = 1;
 				}
@@ -64,7 +75,7 @@ public class Piloto extends JFrame {
 			System.out.println("no hay datos");
 		} else {
 			for (ClasePiloto d : Menu.listaP) {
-				if (d.getIdentificacion() == (asdf.getIdentificacion())) {
+				if (d.getIdentificacion().equals(asdf.getIdentificacion())) {
 					a = Menu.listaP.indexOf(d);
 					Menu.listaP.set(a, asdf);
 					System.out
@@ -88,13 +99,15 @@ public class Piloto extends JFrame {
 			System.out.println("ni hay datos");
 		} else {
 			for (ClasePiloto c : Menu.listaP) {
-				if (c.getIdentificacion() == (asdfg.getIdentificacion())) {
+				if (c.getIdentificacion().equals(asdfg.getIdentificacion())) {
 					a = Menu.listaP.indexOf(c);
 					Menu.listaP.remove(a);
 					System.out.println("el piloto a sido eliminado");
 					JOptionPane.showMessageDialog(null,
 							"el piloto a sido eliminado");
-					// aca debe hacerse el break para que no se siga comparando, aqui ya se saldria definitivamente del ciclo se encuentra el numero de documento
+					// aca debe hacerse el break para que no se siga comparando,
+					// aqui ya se saldria definitivamente del ciclo se encuentra
+					// el numero de documento
 					break;
 
 				}
@@ -116,11 +129,22 @@ public class Piloto extends JFrame {
 	JButton B1, b2, b3, b4, B0;
 	JPanel panelNorte, contenedor, panelcentro, panelsur, paneloeste,
 			paneleste;
-	JTable tabla;
+	
 
 	public Piloto() {
 		super("Crear Piloto");
 		String com[] = { "BCA", "MCA", "PPC", "CIC" };
+		MyTableModel myModel = new MyTableModel();
+		
+		
+		
+	
+		JTable table = new JTable(myModel);
+		table.setPreferredScrollableViewportSize(new Dimension(200, 100));
+		// Creatamos un contenedor para la Tabla
+		JScrollPane scrollPane = new JScrollPane(table);
+		// Agregamos nuestra tabla al contenedor
+		getContentPane().add(scrollPane, BorderLayout.SOUTH);
 		contenedor = new JPanel();
 		panelNorte = new JPanel();
 		panelsur = new JPanel();
@@ -135,21 +159,21 @@ public class Piloto extends JFrame {
 		label1 = new JLabel("nombre piloto");
 		Text1 = new JTextField(20);
 		b2 = new JButton("buscar piloto");
-
-	
+		
+		
+		
 		label3 = new JLabel("Escuderia");
 		combo1 = new JComboBox(com);
 		b4 = new JButton("eliminar piloto");
 		
-		
 		label2 = new JLabel("licencia");
 		txt2 = new JTextField();
 		b3 = new JButton("actualizar piloto");
+
+
 		
-		tabla = new JTable();
 
-	
-
+		
 		
 
 		contenedor.setLayout(new BorderLayout());
@@ -160,85 +184,74 @@ public class Piloto extends JFrame {
 		panelcentro.add(Text0);
 		panelcentro.add(label1);
 		panelcentro.add(Text1);
-		panelcentro.add(label2);
-		panelcentro.add(txt2);
 		panelcentro.add(label3);
 		panelcentro.add(combo1);
+		panelcentro.add(label2);
+		panelcentro.add(txt2);
+		
 		panelcentro.add(B1);
 		panelcentro.add(b2);
 		panelcentro.add(b3);
 		panelcentro.add(b4);
-		panelcentro.add(tabla);
-		
+		//panelcentro.add(table);
+
 		B1.addActionListener(new ActionListener() {
-			
+
 			public void actionPerformed(ActionEvent e) {
-			
-				int idpiloto;
+
+				String idpiloto;
 				String nombrepiloto, escuderia, licencia;
 				Piloto ca = new Piloto();
-				
-				
-				idpiloto = Text0.getText().length();
+
+				idpiloto = Text0.getText();
 				nombrepiloto = Text1.getText();
 				escuderia = combo1.getSelectedItem().toString();
 				licencia = txt2.getText();
-				
 
-				ClasePiloto ccc = new ClasePiloto(idpiloto,
-						nombrepiloto, escuderia, licencia);
-				
-				ca.igualesEs(ccc);
+				ClasePiloto ccc = new ClasePiloto(idpiloto, nombrepiloto,
+						escuderia, licencia);
+
+				ca.iguales(ccc);
 
 				Text0.setText("");
 				Text1.setText("");
 				txt2.setText("");
-				
 
 			}
 		});
 
-		
 		b2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int idpiloto;
+				String idpiloto;
 				Piloto ca = new Piloto();
-				idpiloto = Text0.getText().length();
-				ClasePiloto cc = new ClasePiloto(idpiloto, "", "",
-						"");
-				ca.buscarE(cc);				
-				
+				idpiloto = Text0.getText();
+				ClasePiloto cc = new ClasePiloto(idpiloto, "", "", "");
+				ca.buscarE(cc);
+
 				for (ClasePiloto a : Menu.listaP) {
-					Text1.setText(String.valueOf(a.getNombrePiloto()
-							.toString()));
+					Text1.setText(String
+							.valueOf(a.getNombrePiloto().toString()));
 					combo1.setSelectedItem(String.valueOf(a.getEscuderia()
 							.toString()));
-					txt2.setText(String.valueOf(a.getLicencia()
-							.toString()));
-					
+					txt2.setText(String.valueOf(a.getLicencia().toString()));
+
 				}
 			}
 		});
-		
-		
-		
-		
-		
+
 		b4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int idpiloto;
+				String idpiloto;
 				String nombrepiloto, escuderia, licencia;
 				Piloto ca = new Piloto();
-				
-				
-				idpiloto = Text0.getText().length();
+
+				idpiloto = Text0.getText();
 				nombrepiloto = Text1.getText();
 				escuderia = combo1.getSelectedItem().toString();
 				licencia = txt2.getText();
-				
 
-				ClasePiloto ccc = new ClasePiloto(idpiloto,
-						nombrepiloto, escuderia, licencia);
+				ClasePiloto ccc = new ClasePiloto(idpiloto, nombrepiloto,
+						escuderia, licencia);
 				ca.eliminarEs(ccc);
 
 				Text1.setText(""); // se dejan en blanco las cajas de texto
@@ -250,23 +263,19 @@ public class Piloto extends JFrame {
 			}
 		});
 
-		
-		
 		b3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				int idpiloto;
+				String idpiloto;
 				String nombrepiloto, escuderia, licencia;
 				Piloto ca = new Piloto();
-				
-				
-				idpiloto = Text0.getText().length();
+
+				idpiloto = Text0.getText();
 				nombrepiloto = Text1.getText();
 				escuderia = combo1.getSelectedItem().toString();
 				licencia = txt2.getText();
-				
 
-				ClasePiloto ccc = new ClasePiloto(idpiloto,
-						nombrepiloto, escuderia, licencia);
+				ClasePiloto ccc = new ClasePiloto(idpiloto, nombrepiloto,
+						escuderia, licencia);
 				ca.actualizarEs(ccc);
 
 				Text0.setText("");
@@ -277,18 +286,6 @@ public class Piloto extends JFrame {
 			}
 		});
 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-
 		// se le agrega al contenedor todos los paneles en diferentes partes del
 		// frame para una mejor distribucion del los componentes
 		contenedor.add(panelcentro, BorderLayout.CENTER);
@@ -297,7 +294,84 @@ public class Piloto extends JFrame {
 		contenedor.add(paneloeste, BorderLayout.WEST);
 		contenedor.add(paneleste, BorderLayout.EAST);
 		add(contenedor);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				System.exit(0);
+			}
+		});
+	}
+		
+		class MyTableModel extends AbstractTableModel {
+			final String[] columnNames = { "Identificacion", "Nombre", "Escuderia",
+					"licencia" };
+			
+			
+			final Object[][] data = {
+					{ "Mary", "Campione", "Esquiar", new Integer(5),
+							new Boolean(false) },
+					{ "Lhucas", "Huml", "Patinar", new Integer(3),
+							new Boolean(true) },
+					{ "Kathya", "Walrath", "Escalar", new Integer(2),
+							new Boolean(false) },
+					{ "Marcus", "Andrews", "Correr", new Integer(7),
+							new Boolean(true) } };
 
+			// únicamente retornamos el numero de elementos del
+			// array de los nombres de las columnas
+			public int getColumnCount() {
+				return columnNames.length;
+			}
+
+			// retormanos el numero de elementos
+			// del array de datos
+			public int getRowCount() {
+				return data.length;
+			}
+
+			// retornamos el elemento indicado
+			public String getColumnName(int col) {
+				return columnNames[col];
+			}
+
+			// y lo mismo para las celdas
+			public Object getValueAt(int row, int col) {
+				return data[row][col];
+			}
+
+			/*
+			 * Este metodo sirve para determinar el editor predeterminado para cada
+			 * columna de celdas
+			 */
+			public Class getColumnClass(int c) {
+				return getValueAt(0, c).getClass();
+			}
+
+			/*
+			 * 
+			 * /* No tienes que implementar este método a menos que los datos de tu
+			 * tabla cambien
+			 */
+			public void setValueAt(Object value, int row, int col) {
+				data[row][col] = value;
+				fireTableCellUpdated(row, col);
+			}
+
+		}
+		
+
+		
+
+		
+
+
+	
+	
+	public static void main(String[] args) {
+		Piloto frame = new Piloto();
+		frame.pack();
+		frame.setVisible(true);
+		frame.setSize(400, 400);
 	}
 
 }
