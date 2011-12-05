a2e2d72cf9bc0fc30b300ec4add832a72f316f94
fbotero@heinsohn.com.co <fbotero@heinsohn.com.co@e3a33706-27ed-7515-997f-11477e519433>
2011-12-05 14:47:27 +0000

    las jtable estn listas solo queda llenar el arrylist de campeonato

--- trunk/menus/src/Campeonato.java
+++ trunk/menus/src/Campeonato.java
@@ -1,14 +1,20 @@
+import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
+
+import javax.swing.table.AbstractTableModel;
 
 public class Campeonato extends JFrame {
 
@@ -33,89 +39,11 @@ public class Campeonato extends JFrame {
 		JOptionPane.showMessageDialog(null, "Campeonato Creado con Exito");
 	}
 
-	public void actualizar(ClaseCampeonato ca2) {
-		int a = 0;
-		if (Menu.listaC.isEmpty()) {
-			System.out.println("no ahi datos");
-		} else {
-			for (ClaseCampeonato b : Menu.listaC) {
-				if (b.getIdCampeonato().equals(ca2.getIdCampeonato())) {
-					a = Menu.listaC.indexOf(b);
-					Menu.listaC.set(a, ca2);
-					System.out
-							.println("los datos se han actualizado con exito");
-					JOptionPane.showMessageDialog(null,
-							"los datos se han actualizado cone exito");
-				} else {
-					System.out
-							.println("no existe ningun campeonato con ese id");
-				}
-			}
-
-		}
-
-	}
-
-	public void buscarcampeonato(ClaseCampeonato ad) {
-		int oe = 0;
-		// con el isempty si mira si el arraylist esta vacio nos muestra en
-		// pantalla el mensaje
-		if (Menu.listaC.isEmpty()) {
-			System.out.println("no hay datos");
-
-			// se crea un for que me recorre todo el indice de el arraylist y me
-			// compara si loq ingresa el usuario es igual a algun indice del
-			// arraylist muestra en pantalla los datos de ese indice
-		} else {
-			for (ClaseCampeonato c : Menu.listaC) {
-				if (c.getIdCampeonato().equals(ad.getIdCampeonato())) {
-					System.out.println(c.toString());
-					oe = 1;
-				}
-			}
-
-			// si no encuentra ninguno
-			if (oe == 0)
-				System.out.println("No Existe el Campeonato");
-
-		}
-
-	}
-
-	public void eliminarEs(ClaseCampeonato asdfg) {
-		int a = -1;
-		if (Menu.listaC.isEmpty()) {
-			System.out.println("ni hay datos");
-		} else {
-			for (ClaseCampeonato c : Menu.listaC) {
-				if (c.getIdCampeonato().equals(asdfg.getIdCampeonato())) {
-					a = Menu.listaC.indexOf(c);
-					Menu.listaC.remove(a);
-					System.out.println("el Campeonato ha sido eliminado");
-					JOptionPane.showMessageDialog(null,
-							"el campeonato ha sido eliminado");
-					// aca debe hacerse el break para que no se siga comparando,
-					// aqui ya se saldria definitivamente del ciclo se encuentra
-					// el numero de documento
-					break;
-
-				}
-			}
-
-		}
-
-		if (a == -1) {
-			System.out.println("no existe ningun campeonato con ese id");
-
-		}
-
-	}
-
 	private JTextField jTCodigo, JTnombre;
 	// Estas son las variables para las eqtiquetas de las cajas de texto
 	private JLabel lblCodigo, lblInombre;
 	// Estas son las variables para los botones
-	private JButton insertar, eliminar, actualizar, buscar;
+	private JButton insertar;
 	// private JButton insertar, eliminar, actualizar, buscar;
 	// Esta es la cuadricula que me permite ubicar los elementos de la ventana
 	private GridLayout cuadricula;
@@ -134,7 +62,7 @@ public class Campeonato extends JFrame {
 		// llamada a super para el nombre de la ventana
 		super("Campeonato");
 		// Instancio la cuadrivula
-		cuadricula = new GridLayout(5, 2);
+		cuadricula = new GridLayout(6, 2);
 		// Asigno el contenedor
 		contenedor = getContentPane();
 		// al contenedor le agrego la cuadricula
@@ -152,19 +80,6 @@ public class Campeonato extends JFrame {
 		insertar = new JButton("Insertar");
 		// Asigno el evento al boton insertar
 
-		// Creo el boton eliminar
-		eliminar = new JButton("Eliminar");
-		// Asigno el evento al boton eliminar
-
-		// Creo el boton actualizar
-		actualizar = new JButton("Actualizar");
-		// Asigno el evento al boton actualizar
-
-		// Agrego al contenedor la etiqueta nombre
-
-		buscar = new JButton("Buscar");
-		// Asigno el evento al boton buscar
-
 		contenedor.add(lblCodigo);
 		// Agrego al contenedor la caja de texto nombre
 		contenedor.add(jTCodigo);
@@ -174,12 +89,6 @@ public class Campeonato extends JFrame {
 		contenedor.add(JTnombre);
 		// Agrego al contenedor la etiqqueta de producto
 		contenedor.add(insertar);
-		// Agrego al contenedor la caja de texto cantidad
-		contenedor.add(eliminar);
-		// Agrego al contenedor la etiqueeta de precio
-		contenedor.add(actualizar);
-		// Agrego al contenedor la caja de texto precio
-		contenedor.add(buscar);
 		// Agrego al contenedor el panel
 		contenedor.add(panel);
 		// Dimensiono la ventana
@@ -207,68 +116,133 @@ public class Campeonato extends JFrame {
 			}
 		});
 
-		eliminar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
+		class MyTableModel extends AbstractTableModel {
+			final String[] name = { "Identificacion", "Nombre", "Escuderia",
+					"Licencia", "Seleccionado" };
 
-				String codigoCampeonato, nombreCampeonato;
+			Object[][] objects = new Object[Menu.listaP.size()][5];
+			{
 
-				Campeonato ca = new Campeonato();
-
-				codigoCampeonato = jTCodigo.getText();
-				nombreCampeonato = JTnombre.getText();
-
-				ClaseCampeonato ccc = new ClaseCampeonato(codigoCampeonato,
-						nombreCampeonato);
-
-				ca.eliminarEs(ccc);
-
-				jTCodigo.setText("");
-				JTnombre.setText("");
-
-			}
-		});
-
-		buscar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				String idCampeonato, nombre;
-				Campeonato ca = new Campeonato();
-				idCampeonato = jTCodigo.getText();
-				nombre = JTnombre.getText();
-				ClaseCampeonato cc = new ClaseCampeonato(idCampeonato, nombre);
-				ca.buscarcampeonato(cc);
-
-				for (ClaseCampeonato a : Menu.listaC) {
-
-					jTCodigo.setText(String.valueOf(a.getIdCampeonato()
-							.toString()));
-					JTnombre.setText(String.valueOf(a.getNombreCampeonato()
-							.toString()));
+				for (int i = 0; i < Menu.listaP.size(); i++) {
+					objects[i][0] = Menu.listaP.get(i).getIdentificacion();
+					objects[i][1] = Menu.listaP.get(i).getNombrePiloto();
+					objects[i][2] = Menu.listaP.get(i).getEscuderia();
+					objects[i][3] = Menu.listaP.get(i).getLicencia();
+					objects[i][4] = new Boolean(false);
 
 				}
 			}
-		});
 
-		actualizar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-
-				String codigoCampeonato, nombreCampeonato;
-
-				Campeonato ca = new Campeonato();
-
-				codigoCampeonato = jTCodigo.getText();
-				nombreCampeonato = JTnombre.getText();
-
-				ClaseCampeonato ccc = new ClaseCampeonato(codigoCampeonato,
-						nombreCampeonato);
-
-				ca.actualizar(ccc);
-
-				jTCodigo.setText("");
-				JTnombre.setText("");
-
+			// únicamente retornamos el numero de elementos del
+			// array de los nombres de las columnas
+			public int getColumnCount() {
+				return name.length;
 			}
-		});
+
+			public String getColumnName(int column) {
+				return name[column];
+			}
+
+			public int getRowCount() {
+				return objects.length;
+			}
+
+			public Object getValueAt(int row, int column) {
+				return objects[row][column];
+			}
+
+			public Class getColumnClass(int column) {
+				return (getValueAt(0, column).getClass());
+			}
+
+			public void setValueAt(Object value, int row, int column) {
+				objects[row][column] = value;
+			}
+
+			public boolean isCellEditable(int row, int column) {
+				return (column != 5);
+			}
+			
+		}
+		
+		
+		final MyTableModel myModel = new MyTableModel();
+		JTable table = new JTable(myModel);
+	
+		
+		// Creatamos un contenedor para la Tabla
+		JScrollPane scrollPane = new JScrollPane(table);
+		// Agregamos nuestra tabla al contenedor
+		getContentPane().add(scrollPane, BorderLayout.SOUTH);
+
+	
+		
+	
+	       table.setVisible(true);
+
+
+		class MyTableModelCarrera extends AbstractTableModel {
+
+			final String[] name = { "Codigo", "Fecha", "Campeonato", "Vueltas",
+					"Circuito", "Seleccionado" };
+
+			Object[][] objects = new Object[Menu.listaR.size()][6];
+			{
+
+				for (int i = 0; i < Menu.listaR.size(); i++) {
+					objects[i][0] = Menu.listaR.get(i).getIdCarrera();
+					objects[i][1] = Menu.listaR.get(i).getFecha();
+					objects[i][2] = Menu.listaR.get(i).getNombre();
+					objects[i][3] = Menu.listaR.get(i).getNumvueltas();
+					objects[i][4] = Menu.listaR.get(i).getCircuito();
+					objects[i][5] = new Boolean(false);
+
+				}
+			}
+
+			// únicamente retornamos el numero de elementos del
+			// array de los nombres de las columnas
+			public int getColumnCount() {
+				return name.length;
+			}
+
+			public String getColumnName(int column) {
+				return name[column];
+			}
+
+			public int getRowCount() {
+				return objects.length;
+			}
+
+			public Object getValueAt(int row, int column) {
+				return objects[row][column];
+			}
+
+			public Class getColumnClass(int column) {
+				return (getValueAt(0, column).getClass());
+			}
+
+			public void setValueAt(Object value, int row, int column) {
+				objects[row][column] = value;
+			}
+
+			public boolean isCellEditable(int row, int column) {
+				return (column != 6);
+			}
+
+		}
+
+		MyTableModelCarrera myMod = new MyTableModelCarrera();
+
+		JTable tablaCarrera = new JTable(myMod);
+		
+		tablaCarrera.setPreferredScrollableViewportSize(new Dimension(500, 70));
+
+		JScrollPane scrollPane2 = new JScrollPane(tablaCarrera);
+
+		getContentPane().add(scrollPane2, BorderLayout.CENTER);
+
+		tablaCarrera.setVisible(true);
 
 	}
-
 }
